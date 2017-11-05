a88142514a5f5e1876149209cc12e4b7029fc992
isurutmv <isurutmv@gmail.com>
2017-11-05 02:46:45 -0800

    Creating Fils Structure

---
+++ application/views/index.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	</head>
+	<body>
+		 <a href="index.php/Welcome/Admin" class="btn btn-primary btn-lg active" role="button">Admin View</a>
+		 <a href="index.php/Welcome/Manager" class="btn btn-primary btn-lg active" role="button">Manager View</a>
+		 <a href="index.php/Welcome/User" class="btn btn-primary btn-lg active" role="button">User View</a>
+
+	</body>
